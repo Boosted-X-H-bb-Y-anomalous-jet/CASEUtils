8ab13816b70fc1cae124f32683467f1b09ee2feb
Matej Roguljic <matej.roguljic@gmail.com>
2024-06-03 05:57:06 -0500

    adapt for el8

--- H5_maker/tar_env.sh
+++ H5_maker/tar_env.sh
@@ -1,6 +1,6 @@
 # capture the current directory in tarball
 WD=$(pwd)
 cd $CMSSW_BASE/../
-tar --exclude-caches-all --exclude-vcs --exclude-caches-all --exclude-vcs --exclude="tmp" --exclude=".scram" --exclude=".SCRAM" --exclude="CMSSW_11_3_4/src/CASEUtils/H5_maker/logs" --exclude="CMSSW_11_3_4/src/CASEUtils/H5_maker/*.h5" --exclude="CMSSW_11_3_4/src/CASEUtils/jet_images" --exclude="CMSSW_11_3_4/src/TagNTrain" --exclude="CMSSW_11_3_4/src/H5_Storage" --exclude="CMSSW_11_3_4/src/2DAlphabet" --exclude="CMSSW_11_3_4/src/CombineHarvester" --exclude="CMSSW_11_3_4/src/HiggsAnalysis" --exclude="CMSSW_11_3_4/src/twoD-env" -cvzf H5_env.tgz CMSSW_11_3_4 
-xrdcp -f H5_env.tgz root://cmseos.fnal.gov//store/user/$USER/H5_env.tgz
+tar --exclude-caches-all --exclude-vcs --exclude-caches-all --exclude-vcs --exclude="tmp" --exclude=".scram" --exclude=".SCRAM" --exclude="CMSSW_12_3_5/src/CASEUtils/H5_maker/logs" --exclude="CMSSW_12_3_5/src/CASEUtils/H5_maker/*.h5" -cvzf el8_H5_env.tgz CMSSW_12_3_5 
+xrdcp -f el8_H5_env.tgz root://cmseos.fnal.gov//store/user/$USER/el8_H5_env.tgz
 cd ${WD}
